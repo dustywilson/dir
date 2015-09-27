ffd92a0b67a4f3d134d27e33008fe7ff868f4508
Dusty Wilson <dusty@linux.com>
2015-09-27 03:59:21 -0700

    Updated ex1 for file.CreateFile and directory.FindFiles examples

--- examples/ex1/ex1.go
+++ examples/ex1/ex1.go
@@ -4,23 +4,34 @@ import (
 	"fmt"
 	"regexp"
 
-	"github.com/dustywilson/dir"
 	"github.com/dustywilson/dir/directory"
+	"github.com/dustywilson/dir/file"
 )
 
 func main() {
 	root := directory.CreateDirectory(nil, "My Root Directory")
 
 	directory.CreateDirectory(root, "Fruit").CreateDirectory("Green").CreateDirectory("Grapes").CreateDirectory("Sour")
-	directory.CreateDirectory(root, "Grains").CreateDirectory("Wheat")
-	directory.CreateDirectory(root, "Dessert").CreateDirectory("Cake")
+	file.CreateFile(directory.CreateDirectory(root, "Meat").CreateDirectory("Cow"), "Ground Chuck")
+	cake := directory.CreateDirectory(root, "Dessert").CreateDirectory("Cake")
+
+	file.CreateFile(cake, "Angel Food")
 
 	dirs, err := root.FindDirectories(regexp.MustCompile(`[Rr]`), -1)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Matched %d directories.\n", len(dirs))
+	fmt.Printf("\nMatched %d directories.\n", len(dirs))
 	for _, d := range dirs {
-		fmt.Println(dir.Path(d))
+		fmt.Println(d)
+	}
+
+	files, err := root.FindFiles(regexp.MustCompile(`[Oo]`), -1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nMatched %d files.\n", len(files))
+	for _, f := range files {
+		fmt.Println(f)
 	}
 }
