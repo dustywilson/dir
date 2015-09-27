46671ba7d7dd3efbb572aa8db825ee68a80736a9
Dusty Wilson <dusty@linux.com>
2015-09-27 03:03:02 -0700

    Added dir.ErrIsCurrentVersion

--- errors.go
+++ errors.go
@@ -4,8 +4,9 @@ import "errors"
 
 // Errors
 var (
-	ErrIsRoot   = errors.New("is root")
-	ErrNoMatch  = errors.New("no match")
-	ErrNotEmpty = errors.New("not empty")
-	ErrExists   = errors.New("already exists")
+	ErrIsRoot           = errors.New("is root")
+	ErrNoMatch          = errors.New("no match")
+	ErrNotEmpty         = errors.New("not empty")
+	ErrExists           = errors.New("already exists")
+	ErrIsCurrentVersion = errors.New("is set as current version")
 )
